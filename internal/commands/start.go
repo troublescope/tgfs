6abf81fbbcdc673d45f0161b4e3580dee3f1d6d3
tomyprs <tomiprs25@gmail.com>
2024-02-27 21:30:34 +0700

    start: add `inlineButton`
    Signed-off-by: tomyprs <tomiprs25@gmail.com>

--- internal/commands/start.go
+++ internal/commands/start.go
@@ -8,6 +8,7 @@ import (
 	"github.com/celestix/gotgproto/dispatcher/handlers"
 	"github.com/celestix/gotgproto/ext"
 	"github.com/celestix/gotgproto/storage"
+	"github.com/gotd/td/tg"
 )
 
 func (m *command) LoadStart(dispatcher dispatcher.Dispatcher) {
@@ -22,10 +23,28 @@ func start(ctx *ext.Context, u *ext.Update) error {
 	if peerChatId.Type != int(storage.TypeUser) {
 		return dispatcher.EndGroups
 	}
+	replyMarkup := &tg.ReplyInlineMarkup{
+			Rows: []tg.KeyboardButtonRow{
+				{
+					Buttons: []tg.KeyboardButtonClass{
+						&tg.KeyboardButtonURL{
+							Text: "Support Groups!",
+							URL:  "https://t.me/+2u67mVOGziU5YzRh",
+						},
+						&tg.KeyboardButtonURL{
+							Text: "Join Channel!",
+							URL:  "https://kai_verse.t.me",
+						},
+					},
+				},
+			},
+		}
+	opts := &ext.ReplyOpts{Markup: replyMarkup}
 	if len(config.ValueOf.AllowedUsers) != 0 && !utils.Contains(config.ValueOf.AllowedUsers, chatId) {
-		ctx.Reply(u, "You are not allowed to use this bot.", nil)
+		
+		ctx.Reply(u, "You are not allowed to use this bot.", opts)
 		return dispatcher.EndGroups
 	}
-	ctx.Reply(u, "Hi, send me any file to get a direct streamble link to that file.", nil)
+	ctx.Reply(u, "Hi, send me any file to get a direct streamable link to that file.", opts)
 	return dispatcher.EndGroups
 }
